24c9348150ad6932be28599e8eda2fbc15a7b697
jalvaro77 <j_alvaro_7@hotmail.com>
2020-08-12 16:41:49 -0500

    Actualizacion

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -11,7 +11,7 @@ class Course(models.Model):
      responsible_id = fields.Many2one(
         'res.users', string="Responsible", 
         index=True, ondelete='set null')
-    session_ids = fields.One2many('openacademy.session', 'course_id')
+     session_ids = fields.One2many('openacademy.session', 'course_id')
 
 class Session(models.Model):
     _name = 'openacademy.session'
