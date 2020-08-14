cd681103948f9b578b33efd96888e0162bbaaa5c
jalvaro77 <j_alvaro_7@hotmail.com>
2020-08-14 16:51:15 -0500

    Actualizacion

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class Course(models.Model):
      _name = 'openacademy.course'
+     _description="Modelos para cursos"
      name = fields.Char(string="Title",required=True)
      description = fields.Text()
 
@@ -15,14 +16,15 @@ class Course(models.Model):
 
 class Session(models.Model):
     _name = 'openacademy.session'
-
+    _description="Modelos para sesiones"
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
---
+++ openacademy/models/partner.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    # Add a new column to the res.partner model, by default partners are not
+    # instructors
+    instructor = fields.Boolean("Instructor", default=False)
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Attended Sessions",
+        readonly=True)
+
+    other_field = fields.Boolean(default=True)
+    other_field2 = fields.Boolean(default=True)
