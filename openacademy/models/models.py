3dd155cfb578d4861525d85b03ecfbdfa4449bde
jalvaro77 <j_alvaro_7@hotmail.com>
2020-08-20 16:41:04 -0500

    Vistas Avanzadas

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 import time
 
@@ -31,7 +31,7 @@ class Course(models.Model):
 
      def copy(self, default=None):
         if default is None:
-        default = {}
+            default = {}
 
         copied_count = self.search_count(
             [('name', 'ilike', 'Copy of %s%% %' (self.name))])
@@ -62,6 +62,17 @@ class Session(models.Model):
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats', store=True)
     active = fields.Boolean(default=True)
 
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+
+    hours = fields.Float(string="Duration in hours",
+                         compute='_get_hours', inverse='_set_hours')
+
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
+
+    color = fields.Integer()
+
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
@@ -88,11 +99,47 @@ class Session(models.Model):
             }
         self.active = True
 
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self.filtered('start_date'):
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self.filtered('start_date'):
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)            
+            
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for record in self.filtered('instructor_id'):
             if record.instructor_id  in record.attendee_ids:
                 raise exceptions.ValidationError(
                         "A session's instructor can't be an attendee")
-
-
